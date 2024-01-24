a4641e277b872bb7945fbb9e1a4103d2626dbdd7
kirkarr <95094277+kirkarr@users.noreply.github.com>
2024-01-24 22:15:03 +0300

    Create startup.lua
    This file will install or startup OS main file.

---
+++ startup.lua
@@ -0,0 +1,44 @@
+local sx,sy = term.getSize()
+term.clear()
+if not fs.exists("os/config.json") then
+	term.setCursorPos(1,1)
+	term.write("OS running first time")
+	term.setCursorPos(1,2)
+	term.write("Collecting data")
+	local datareq,reason = http.get("https://raw.githubusercontent.com/kirkarr/KirkaOS/main/config.json")
+	sleep(2)
+	if datareq ~= nil then
+		local data = unserializeJSON(datareq.readAll())
+		datareq.close()
+		fs.makeDir("os")
+		for i=1,#data["Files"] do
+			filedata = data["Files"][i]
+			term.clear()
+			term.setCursorPos(1,1)
+			term.write("Loading files")
+			term.setCursorPos(1,2)
+			term.write("Loading "..filedata["Name"])
+			if filedata["Url"] ~= "" then
+				file = fs.open("os/"..filedata["Name"],"w+")
+				req = http.get(filedata["Url"])
+				content = req.readAll()
+				file.write(content)
+				req.close()
+				file.close()
+			end
+			sleep(3)
+		end
+		local cfg = fs.open("os/data.json","w+")
+		cfg.write(serializeJSON(data))
+		cfg.close()
+		os.rebo0t()
+	else
+		term.clear()
+		term.setCursorPos(1,1)
+		term.write("Cannot load OS")
+		term.setCursorPos(1,2)
+		term.write("Reason: "..reason)
+	end
+else
+	os.run("os/main.lua")
+end
